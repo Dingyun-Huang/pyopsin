61a6b701a93acf3e4cffe3b9bd2d1cbc8e05770e
Dingyun Huang <dh582@cam.ac.uk>
2023-05-13 11:44:59 +0100

    parseToSmiles implemented witth NameToStructure.

--- pyopsin/pyopsin.py
+++ pyopsin/pyopsin.py
@@ -8,11 +8,15 @@ class NameToStructure:
         self.path = path
 
     def name_to_smiles(self, name):
-        jpype.startJVM()
-        return None
+        jpype.startJVM(classpath=[self.path])
+        from uk.ac.cam.ch.wwmm import opsin
+        nts = opsin.NameToStructure.getInstance()
+        smiles = nts.parseToSmiles(name)
+        return smiles
 
 
 if __name__ == "__main__":
-    nts = NameToStructure("E:/PhD/cde_application/pyopsin/opsin_cli.jar")
-    name = "toluene"
-    nts.name_to_smiles(name)
+    nts = NameToStructure("/home/dh582/pyopsin/opsin_cli.jar")
+    name = "biphenyl"
+    smiles = nts.name_to_smiles(name)
+    print(smiles)
