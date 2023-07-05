297d81a031aa5fe3a3c63d4095e71da22eefbac3
DINGYUN HUANG <94184902+Dingyun-Huang@users.noreply.github.com>
2023-07-05 13:49:59 +0100

    Update pyopsin.py
    Added more flags and parse t cml function.

--- pyopsin/pyopsin.py
+++ pyopsin/pyopsin.py
@@ -4,25 +4,49 @@ from jpype.types import *
 
 class NameToSmiles:
     
-    def __init__(self, path):
+    def __init__(self, path, 
+                       allowUninterpretableStereo=False, 
+                       allowRadicals=False,
+                       wildcardRadicals=False,
+                       allowAcidsWithoutAcid=False,):
         self.path = path
 
-    def name_to_smiles(self, name, allowUninterpretableStereo=False):
         if not jpype.isJVMStarted():
             jpype.startJVM(classpath=[self.path])
         from uk.ac.cam.ch.wwmm import opsin
-        nts = opsin.NameToStructure.getInstance()
-        ntsconfig = opsin.NameToStructureConfig()
+
+        self.config = opsin.NameToStructureConfig()
         if allowUninterpretableStereo:
-            ntsconfig.setWarnRatherThanFailOnUninterpretableStereochemistry(True)
-        results = nts.parseChemicalName(name, ntsconfig)
+            self.config.setWarnRatherThanFailOnUninterpretableStereochemistry(True)
+        if allowRadicals:
+            self.config.setAllowRadicals(True)
+        if allowAcidsWithoutAcid:
+            self.config.setInterpretAcidsWithoutTheWordAcid(True)
+        if wildcardRadicals:
+            self.config.setOuputRadicalsAsWildCardAtoms(True)
+        
+        self.nts = opsin.NameToStructure.getInstance()
+
+
+    def name_to_smiles(self, name):
+
+        results = self.nts.parseChemicalName(name, self.config)
         return results.getSmiles()
+    
+    def name_to_cml(self, name):
+
+        results = self.nts.parseChemicalName(name, self.config)
+        return results.getCml()
 
 
 if __name__ == "__main__":
     pass
     #### example use ####
-    # nts = NameToSmiles("E:\PhD\cde_application\pyopsin\opsin_cli.jar")
-    # name = "biphenyl"
-    # smiles = nts.name_to_smiles(name, allowUninterpretableStereo=True)
-    # print(smiles)
+    """
+    nts = NameToSmiles("E:\PhD\cde_application\pyopsin\opsin_cli.jar", allowUninterpretableStereo=True,)
+    name = "2,4,6-trinitrotoluene"
+    smiles = nts.name_to_smiles(name)
+    cml = nts.name_to_cml(name)
+    print(smiles)
+    print(cml)
+    """
