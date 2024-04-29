75fa887723be084651aec4e9e1447e948c23c1fc
Dingyun Huang <dh582@cam.ac.uk>
2024-04-29 17:38:43 +0100

    Enabled GitHub Action

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+from pyopsin import PyOpsin
+
+class TestPyOpsin(unittest.TestCase):
+    def setUp(self):
+        self.pyopsin = PyOpsin()
+        self.name = "2,4,6-trinitrotoluene"
+
+    def test_to_smiles(self):
+        smiles = self.pyopsin.to_smiles(self.name)
+        self.assertIsInstance(smiles, str)
+        self.assertNotEqual(smiles, "[N+](=O)([O-])C1=C(C)C(=CC(=C1)[N+](=O)[O-])[N+](=O)[O-]")
+
+
+if __name__ == '__main__':
+    unittest.main()
